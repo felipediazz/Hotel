3069966c5952167ee3bc2969203ca7447763c81f
JoseOrtizMedel <joi.ortiz@duocuc.cl>
2021-06-27 19:30:36 -0500

    a

--- Django/core/static/core/js/hoteles.js
+++ Django/core/static/core/js/hoteles.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
-    $('#example').DataTable({
-            searchPanes:{
-                cascadePanes:true,
-                dtOpts:{
-                    searching:false
-                }
-            },
-            dom:'Pfrtip',
-            
-    });
-
+$(document).ready(function () {
+  $("#example").DataTable({
+    language: {
+      url: "http://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json",
+      searchPanes: {
+        clearMessage: 'Limpiar',
+        collapse: {0: 'Filtros', _: 'Filtros (%d)'},
+      }
+    },
+    searchPanes: {
+      cascadePanes: true,
+      dtOpts: {
+        searching: false,
+      },
+      
+    },
+    buttons: ["searchPanes"],
+    dom: "Bfrtip",
+  });
 });
-
